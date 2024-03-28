62169b217c70be3c66f2c262f63a58a9e726e935
mn556112 <136472222+mn556112@users.noreply.github.com>
2024-03-28 21:39:08 +0900

    Add files via upload

--- script/Main.js
+++ script/Main.js
@@ -44,6 +44,16 @@ function calcDate(IsDetail, Type, idk) {
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+        if (distance <! -86400000) {
+            if (IsDetail == true) {
+                return `😭😭😭`;
+
+            } else {
+                return 'D-DAY';;
+            }
+        }
+
+
         if (distance < 0) {
             if (IsDetail == true) {
                 return ``;
